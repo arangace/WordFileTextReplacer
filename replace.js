ec2dd145775e07f9300158e6e5fb111385d7f067
arangace <ahua291@aucklanduni.ac.nz>
2021-11-02 18:04:06 +1300

    working functinoality of word replacer code with a better command line UI

--- replace.js
+++ replace.js
@@ -12,15 +12,22 @@ const userPrompt = () => {
     const replaceInformation = []
     const properties = [
         {
+            name: 'original_file',
+            description: 'Original file name (excluding .docx) Example: coverletter '
+        },
+        {
+
             name: 'company_name',
+            description: 'Name of company',
             validator: /^[a-zA-Z\s\-]+$/,
-            warning: 'Username must be only letters, spaces, or dashes',
+            warning: 'Company Name must be only letters, spaces, or dashes',
             hidden: false
         },
         {
             name: 'job_title',
+            description: 'Job title',
             validator: /^[a-zA-Z\s\-]+$/,
-            warning: 'Username must be only letters, spaces, or dashes',
+            warning: 'Job Title must be only letters, spaces, or dashes',
             hidden: false
         }
     ];
@@ -31,11 +38,14 @@ const userPrompt = () => {
                 return onErr(err);
             }
             console.log('Company data successfully recieved');
+            console.log('  Original file name ' + result.original_file);
             console.log('  Company name: ' + result.company_name);
             console.log('  Job Title: ' + result.job_title);
-            replaceInformation.company_name = result.company_name
-            replaceInformation.job_title = result.job_title
-            console.log(replaceInformation)
+            replaceInformation.original_file = result.original_file;
+            replaceInformation.company_name = result.company_name;
+            replaceInformation.job_title = result.job_title;
+            //console.log(replaceInformation);
+            replace(replaceInformation);
         });
 
     function onErr(err) {
@@ -43,13 +53,13 @@ const userPrompt = () => {
         return 1;
     }
 
-    replace(replaceInformation);
+
 }
 const replace = (replaceInformation) => {
 
     // Load the docx file as binary content
     var content = fs.readFileSync(
-        path.resolve(__dirname, "simple.docx"),
+        path.resolve(__dirname, replaceInformation.original_file + ".docx"),
         "binary"
     );
     var zip = new PizZip(content);
@@ -70,6 +80,6 @@ const replace = (replaceInformation) => {
     var buf = doc.getZip().generate({ type: "nodebuffer" });
 
     // buf is a nodejs buffer, you can either write it to a file or do anything else with it.
-    fs.writeFileSync(path.resolve(__dirname, `${company_name, job_title}-cover-letter.docx`), buf);
+    fs.writeFileSync(path.resolve(__dirname, `${replaceInformation.company_name, replaceInformation.job_title}-cover-letter.docx`), buf);
 }
 userPrompt();
